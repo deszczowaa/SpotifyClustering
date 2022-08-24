80f8e3126eb7651c643e94bab0bf3de5bb527f68
deszczowaa <maja.szrubarczyk@onet.pl>
2022-08-24 11:34:35 +0200

    Updating report

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import os
 from download_playlist import DownloadPlaylist
 from cluster_playlist import ClusterPlaylist
 from create_playlist import CreatePlaylist
+from create_report import PDF
 
 
 # get Spotipy Api Credentials (SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT SECRET and SPOTIPY_REDIRECT_URI)
 # and set as environmental variables
 
 if __name__ == "__main__":
+    os.environ['SPOTIPY_CLIENT_ID'] = "ac6fd439e91046509cb6297fffe57586"
+    os.environ['SPOTIPY_CLIENT_SECRET'] = "9882a9f65c7b42a79dfc6af5ed68ff5b"
+    os.environ['SPOTIPY_REDIRECT_URI'] = "http://127.0.0.1:8080/"
 
     username = input("Username: ")
     playlist_name = input("Which playlist would you like to cluster? Write name:  ")
@@ -33,3 +37,6 @@ if __name__ == "__main__":
     # creates new playlists out of clustered groups
     playlist = CreatePlaylist(sp, username, playlist_name)
     playlist.create_playlist()
+
+    # generates PDF report
+    pdf = PDF(playlist_name)
